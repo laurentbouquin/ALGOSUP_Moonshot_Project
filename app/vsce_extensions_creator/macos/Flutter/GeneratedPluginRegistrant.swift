c1671d101055019a5df6d23f1936661b406a7b11
laurentbouquin <71769489+laurentbouquin@users.noreply.github.com>
2024-07-29 13:07:53 +0200

    Create the foundation of the app

---
+++ app/vsce_extensions_creator/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,12 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import keypress_simulator_macos
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  KeypressSimulatorMacosPlugin.register(with: registry.registrar(forPlugin: "KeypressSimulatorMacosPlugin"))
+}
