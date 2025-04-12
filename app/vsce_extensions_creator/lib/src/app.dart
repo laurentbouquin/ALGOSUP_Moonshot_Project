68b6776bcefd9a5bfaa9a6f4388619b58b981fbe
laurentbouquin <71769489+laurentbouquin@users.noreply.github.com>
2025-04-12 18:29:31 +0200

    remove useless import

--- app/vsce_extensions_creator/lib/src/app.dart
+++ app/vsce_extensions_creator/lib/src/app.dart
@@ -1,5 +1,3 @@
-import 'dart:ffi';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:vsce_extensions_creator/src/constants/variables.dart';
